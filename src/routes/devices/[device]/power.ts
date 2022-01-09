68a81db4473144611b601f968ce30c3fd907b371
Zachary Stence <zachstence@gmail.com>
2022-01-09 12:26:45 -0600

    chore: remove log

--- src/routes/devices/[device]/power.ts
+++ src/routes/devices/[device]/power.ts
@@ -6,8 +6,6 @@ export const put: RequestHandler = async (request) => {
     const device = request.params.device
     const { model, power } = JSON.parse(request.body as string);
 
-    console.log(device, model, power)
-
     try {
         await GoveeClient.setPower(device, model, power)
     } catch (e) {

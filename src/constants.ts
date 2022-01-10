2ebbba8b6de9417e62cfcc54a64339e4045a71a5
Zachary Stence <zachstence@gmail.com>
2022-01-09 21:19:33 -0600

    chore: install env-cmd for development

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,7 @@
 export const BASE_URL = "https://developer-api.govee.com/v1";
 
-export const API_KEY = process.env.GOVEE_API_KEY
+export const API_KEY = process.env.GOVEE_API_KEY;
+
+if (!API_KEY) {
+    throw new Error("Missing environment variable 'GOVEE_API_KEY'");
+}

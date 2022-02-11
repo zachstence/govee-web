e82e60e347f1df008d2096bf5a29542039cba151
Zachary Stence <zachstence@gmail.com>
2022-02-11 13:47:18 -0600

    feat: use secret file instead of environment variable

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,12 @@
+import { readFileSync } from "fs";
+
 export const BASE_URL = "https://developer-api.govee.com/v1";
 
-export const API_KEY = process.env.GOVEE_API_KEY;
-
-if (!API_KEY) {
-    throw new Error("Missing environment variable 'GOVEE_API_KEY'");
+let apiKey: string;
+try {
+    apiKey = readFileSync("./secret/govee-api-key.txt").toString();
+} catch {
+    console.error("Missing required secret govee-api-key.txt");
+    process.exit(1);
 }
+export const API_KEY = apiKey;
